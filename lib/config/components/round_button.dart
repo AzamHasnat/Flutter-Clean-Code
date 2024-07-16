bd3a5fcdcb47367a1050673cce788b45bd16a93a
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2024-07-16 15:18:09 +0600

    create internet exception component

--- lib/config/components/round_button.dart
+++ lib/config/components/round_button.dart
@@ -4,17 +4,18 @@ import 'package:flutter_clean_code/config/color/colors.dart';
 class RoundButton extends StatelessWidget {
   final String title;
   final VoidCallback onPress;
-  const RoundButton({super.key,required this.title, required this.onPress});
+  final double height;
+  const RoundButton({super.key, this.height = 40 ,required this.title, required this.onPress});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onPress,
       child: Container(
-        height: 40,
-        width: MediaQuery.of(context).size.width * 0.4,
-        decoration: const BoxDecoration(
-          color: AppColors.buttonColor
+        height: height,
+        decoration: BoxDecoration(
+          color: AppColors.buttonColor,
+          borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
           child: Text(title),
